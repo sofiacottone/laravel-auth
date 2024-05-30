25f55fe2ed38731635f8b70ce81f3ae475dda68f
sofiacottone <cottonesofia@outlook.it>
2024-05-30 19:39:44 +0200

    feat: add show view to display single resource

---
+++ resources/views/admin/projects/show.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.admin')
+
+@section('content')
+    <div>
+        <div class="hstack justify-content-between">
+            <h2>{{ $project->name }}</h2>
+            <div>ID: {{ $project->id }}</div>
+        </div>
+        <div>Slug: {{ $project->slug }}</div>
+        <div>Client: {{ $project->client_name }}</div>
+        <div class="mt-5">{{ $project->summary }}</div>
+    </div>
+@endsection

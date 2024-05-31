505400d4aa57e308e488749c93f5dd1326ad4328
sofiacottone <cottonesofia@outlook.it>
2024-05-31 19:23:42 +0200

    feat: add form to add new project

---
+++ resources/views/admin/projects/create.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.admin')
+
+@section('content')
+    <h2 class="mb-3">Add new project</h2>
+
+    <form action="{{ route('admin.projects.store') }}" method="POST">
+        @csrf
+
+        <div class="mb-3">
+            <label for="name" class="form-label">Project name</label>
+            <input type="text" class="form-control" id="name" name="name" value="{{ old('name') }}">
+            @error('name')
+                <div class="text-danger ps-2">{{ $message }}</div>
+            @enderror
+        </div>
+        <div class="mb-3">
+            <label for="client-name" class="form-label">Client name</label>
+            <input type="text" class="form-control" id="client-name" name="client_name" value="{{ old('client_name') }}">
+            @error('client_name')
+                <div class="text-danger ps-2">{{ $message }}</div>
+            @enderror
+        </div>
+        <div class="mb-3">
+            <label for="summary" class="form-label">Summary</label>
+            <textarea class="form-control" id="summary" rows="10" name="summary">{{ old('summary') }}</textarea>
+            @error('summary')
+                <div class="text-danger ps-2">{{ $message }}</div>
+            @enderror
+        </div>
+
+        <button type="submit" class="btn btn-primary">Add</button>
+    </form>
+@endsection

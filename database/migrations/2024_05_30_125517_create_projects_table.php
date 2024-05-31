c87d29809ea8225b70d012b22b7f945c3f0a622f
sofiacottone <cottonesofia@outlook.it>
2024-05-31 19:24:46 +0200

    fix: add unique feat to name column

--- database/migrations/2024_05_30_125517_create_projects_table.php
+++ database/migrations/2024_05_30_125517_create_projects_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->unique();
             $table->string('slug');
             $table->string('client_name', 100)->nullable();
             $table->text('summary')->nullable();

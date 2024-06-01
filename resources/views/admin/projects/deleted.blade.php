46d72d06de01fd654d85bbfcfb134eccd1da5779
sofiacottone <cottonesofia@outlook.it>
2024-06-01 17:27:39 +0200

    feat: add deleted view to display deleted resources

---
+++ resources/views/admin/projects/deleted.blade.php
@@ -0,0 +1,57 @@
+@extends('layouts.admin')
+
+@section('content')
+    <h1>Deleted projects</h1>
+
+    @if (count($projects) > 0)
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Client name</th>
+                    <th>Created</th>
+                    <th class="text-center">Actions</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                @foreach ($projects as $project)
+                    <tr>
+                        <td>{{ $project->id }}</td>
+                        <td>{{ $project->name }}</td>
+                        <td>{{ $project->client_name }}</td>
+                        <td>{{ $project->created_at }}</td>
+
+                        {{-- actions  --}}
+                        <td>
+                            <div class="hstack justify-content-center gap-2 text-center">
+                                <a href="{{ route('admin.projects.show', ['project' => $project->id]) }}"
+                                    class="btn btn-outline-secondary">
+                                    <i class="fa-solid fa-ellipsis-vertical"></i>
+                                </a>
+
+                                <a href="{{ route('admin.projects.edit', ['project' => $project->id]) }}"
+                                    class="btn btn-outline-secondary">
+                                    <i class="fa-solid fa-pencil"></i>
+                                </a>
+
+                                <form action="{{ route('admin.projects.destroy', ['project' => $project->id]) }}"
+                                    method="POST">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" data-project-title="{{ $project->name }}"
+                                        class="js-delete-btn btn btn-outline-danger">
+                                        <i class="fa-solid fa-trash"></i>
+                                    </button>
+                                </form>
+                            </div>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    @else
+        <div>No deleted projects.</div>
+    @endif
+@endsection

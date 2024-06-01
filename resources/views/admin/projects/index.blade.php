6d17a11adcd8205329cf4546599dcebb957b866b
sofiacottone <cottonesofia@outlook.it>
2024-06-01 17:27:00 +0200

    feat: add btn to delete project

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -9,7 +9,7 @@
                 <th>ID</th>
                 <th>Name</th>
                 <th>Client name</th>
-                <th>Created ad</th>
+                <th>Created</th>
                 <th class="text-center">Actions</th>
             </tr>
         </thead>
@@ -24,7 +24,7 @@
 
                     {{-- actions  --}}
                     <td>
-                        <div class="text-center">
+                        <div class="hstack justify-content-center gap-2 text-center">
                             <a href="{{ route('admin.projects.show', ['project' => $project->id]) }}"
                                 class="btn btn-outline-secondary">
                                 <i class="fa-solid fa-ellipsis-vertical"></i>
@@ -34,10 +34,40 @@
                                 class="btn btn-outline-secondary">
                                 <i class="fa-solid fa-pencil"></i>
                             </a>
+
+                            <form action="{{ route('admin.projects.destroy', ['project' => $project->id]) }}" method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" data-project-title="{{ $project->name }}"
+                                    class="js-delete-btn btn btn-outline-danger">
+                                    <i class="fa-solid fa-trash"></i>
+                                </button>
+                            </form>
                         </div>
                     </td>
                 </tr>
             @endforeach
         </tbody>
     </table>
+
+    {{-- modal  --}}
+    <div id="confirmDeleteModal" class="ms-modal">
+
+        {{-- Modal content --}}
+        <div class="ms-modal-content">
+            <div class="hstack align-items-center justify-content-between mb-3">
+                <div class="hstack align-items-center justify-content-center gap-2">
+                    <img src="https://img.icons8.com/ios/40/000000/box-important--v1.png" alt="box-important--v1" />
+                    <div class="fs-3">Confirm delete</div>
+                </div>
+                <div class="ms-close">&times;</div>
+            </div>
+            <div class="ms-modal-body"></div>
+            <div class="hstack justify-content-end gap-2">
+                <button class="ms-close-btn btn btn-secondary">Cancel</button>
+                <button id="modal-confirm" class="btn btn-danger">Delete</button>
+            </div>
+        </div>
+
+    </div>
 @endsection
